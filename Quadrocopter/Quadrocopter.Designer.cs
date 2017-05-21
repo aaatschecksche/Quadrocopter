d25c9b728ef1553688a3d50003ee001d0ae994b9
Matthias Zandanel <matthias.zandl@hotmail.com>
2017-05-21 18:21:18 +0200

    test

--- Quadrocopter/Quadrocopter.Designer.cs
+++ Quadrocopter/Quadrocopter.Designer.cs
@@ -53,9 +53,10 @@
             this.StatusLabel.BackColor = System.Drawing.SystemColors.Highlight;
             this.StatusLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.StatusLabel.Font = new System.Drawing.Font("Arial Black", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.StatusLabel.Location = new System.Drawing.Point(381, 9);
+            this.StatusLabel.Location = new System.Drawing.Point(286, 7);
+            this.StatusLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.StatusLabel.Name = "StatusLabel";
-            this.StatusLabel.Size = new System.Drawing.Size(161, 40);
+            this.StatusLabel.Size = new System.Drawing.Size(121, 33);
             this.StatusLabel.TabIndex = 0;
             this.StatusLabel.Text = "Connecting";
             this.StatusLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -65,9 +66,10 @@
             this.SendButton.BackColor = System.Drawing.Color.Maroon;
             this.SendButton.Cursor = System.Windows.Forms.Cursors.Hand;
             this.SendButton.Font = new System.Drawing.Font("Arial Black", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.SendButton.Location = new System.Drawing.Point(380, 85);
+            this.SendButton.Location = new System.Drawing.Point(285, 69);
+            this.SendButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.SendButton.Name = "SendButton";
-            this.SendButton.Size = new System.Drawing.Size(161, 80);
+            this.SendButton.Size = new System.Drawing.Size(121, 65);
             this.SendButton.TabIndex = 1;
             this.SendButton.Text = "SEND";
             this.SendButton.UseVisualStyleBackColor = false;
@@ -77,11 +79,12 @@
             // 
             this.SendBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.SendBox.DetectUrls = false;
-            this.SendBox.Location = new System.Drawing.Point(147, 137);
+            this.SendBox.Location = new System.Drawing.Point(110, 111);
+            this.SendBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.SendBox.Multiline = false;
             this.SendBox.Name = "SendBox";
             this.SendBox.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
-            this.SendBox.Size = new System.Drawing.Size(227, 24);
+            this.SendBox.Size = new System.Drawing.Size(171, 20);
             this.SendBox.TabIndex = 2;
             this.SendBox.Text = "";
             // 
@@ -91,10 +94,11 @@
             this.RecieveBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.RecieveBox.Cursor = System.Windows.Forms.Cursors.IBeam;
             this.RecieveBox.ForeColor = System.Drawing.Color.Black;
-            this.RecieveBox.Location = new System.Drawing.Point(12, 289);
+            this.RecieveBox.Location = new System.Drawing.Point(9, 235);
+            this.RecieveBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.RecieveBox.Name = "RecieveBox";
             this.RecieveBox.ReadOnly = true;
-            this.RecieveBox.Size = new System.Drawing.Size(416, 451);
+            this.RecieveBox.Size = new System.Drawing.Size(313, 367);
             this.RecieveBox.TabIndex = 3;
             this.RecieveBox.Text = "";
             this.RecieveBox.TextChanged += new System.EventHandler(this.RecieveBox_TextChanged);
@@ -103,10 +107,11 @@
             // 
             this.CommandSelector.Cursor = System.Windows.Forms.Cursors.Hand;
             this.CommandSelector.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.CommandSelector.ItemHeight = 16;
-            this.CommandSelector.Location = new System.Drawing.Point(147, 91);
+            this.CommandSelector.ItemHeight = 13;
+            this.CommandSelector.Location = new System.Drawing.Point(110, 74);
+            this.CommandSelector.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.CommandSelector.Name = "CommandSelector";
-            this.CommandSelector.Size = new System.Drawing.Size(227, 24);
+            this.CommandSelector.Size = new System.Drawing.Size(171, 21);
             this.CommandSelector.TabIndex = 4;
             // 
             // TitleLabel
@@ -114,9 +119,10 @@
             this.TitleLabel.BackColor = System.Drawing.SystemColors.Highlight;
             this.TitleLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.TitleLabel.Font = new System.Drawing.Font("Arial Black", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.TitleLabel.Location = new System.Drawing.Point(12, 9);
+            this.TitleLabel.Location = new System.Drawing.Point(9, 7);
+            this.TitleLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.TitleLabel.Name = "TitleLabel";
-            this.TitleLabel.Size = new System.Drawing.Size(361, 40);
+            this.TitleLabel.Size = new System.Drawing.Size(271, 33);
             this.TitleLabel.TabIndex = 0;
             this.TitleLabel.Text = "Quadrocopter v. 0.04";
             this.TitleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -126,9 +132,10 @@
             this.label1.BackColor = System.Drawing.SystemColors.Highlight;
             this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.label1.Font = new System.Drawing.Font("Arial Black", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 85);
+            this.label1.Location = new System.Drawing.Point(9, 69);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(129, 35);
+            this.label1.Size = new System.Drawing.Size(97, 29);
             this.label1.TabIndex = 0;
             this.label1.Text = "Command";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -138,9 +145,10 @@
             this.label2.BackColor = System.Drawing.SystemColors.Highlight;
             this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.label2.Font = new System.Drawing.Font("Arial Black", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(12, 130);
+            this.label2.Location = new System.Drawing.Point(9, 106);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(129, 35);
+            this.label2.Size = new System.Drawing.Size(97, 29);
             this.label2.TabIndex = 0;
             this.label2.Text = "Data";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -150,9 +158,10 @@
             this.label5.BackColor = System.Drawing.SystemColors.Highlight;
             this.label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.label5.Font = new System.Drawing.Font("Arial Black", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(12, 251);
+            this.label5.Location = new System.Drawing.Point(9, 204);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(129, 35);
+            this.label5.Size = new System.Drawing.Size(97, 29);
             this.label5.TabIndex = 0;
             this.label5.Text = "Log";
             this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -163,9 +172,10 @@
             this.ClearLog.BackColor = System.Drawing.Color.DimGray;
             this.ClearLog.Cursor = System.Windows.Forms.Cursors.Hand;
             this.ClearLog.Font = new System.Drawing.Font("Arial Black", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ClearLog.Location = new System.Drawing.Point(148, 251);
+            this.ClearLog.Location = new System.Drawing.Point(111, 204);
+            this.ClearLog.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.ClearLog.Name = "ClearLog";
-            this.ClearLog.Size = new System.Drawing.Size(113, 35);
+            this.ClearLog.Size = new System.Drawing.Size(85, 28);
             this.ClearLog.TabIndex = 5;
             this.ClearLog.Text = "CLEAR";
             this.ClearLog.UseVisualStyleBackColor = false;
@@ -177,10 +187,11 @@
             this.DebugBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.DebugBox.Cursor = System.Windows.Forms.Cursors.IBeam;
             this.DebugBox.ForeColor = System.Drawing.Color.Black;
-            this.DebugBox.Location = new System.Drawing.Point(438, 289);
+            this.DebugBox.Location = new System.Drawing.Point(328, 235);
+            this.DebugBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.DebugBox.Name = "DebugBox";
             this.DebugBox.ReadOnly = true;
-            this.DebugBox.Size = new System.Drawing.Size(416, 451);
+            this.DebugBox.Size = new System.Drawing.Size(313, 367);
             this.DebugBox.TabIndex = 3;
             this.DebugBox.Text = "";
             // 
@@ -191,9 +202,10 @@
             this.ListeningCheckBox.Cursor = System.Windows.Forms.Cursors.Hand;
             this.ListeningCheckBox.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.ListeningCheckBox.Font = new System.Drawing.Font("Arial Black", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ListeningCheckBox.Location = new System.Drawing.Point(12, 168);
+            this.ListeningCheckBox.Location = new System.Drawing.Point(454, 180);
+            this.ListeningCheckBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.ListeningCheckBox.Name = "ListeningCheckBox";
-            this.ListeningCheckBox.Size = new System.Drawing.Size(194, 63);
+            this.ListeningCheckBox.Size = new System.Drawing.Size(187, 51);
             this.ListeningCheckBox.TabIndex = 6;
             this.ListeningCheckBox.Text = "Listening";
             this.ListeningCheckBox.UseVisualStyleBackColor = true;
@@ -201,9 +213,10 @@
             // 
             // KPButton
             // 
-            this.KPButton.Location = new System.Drawing.Point(685, 91);
+            this.KPButton.Location = new System.Drawing.Point(526, 69);
+            this.KPButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.KPButton.Name = "KPButton";
-            this.KPButton.Size = new System.Drawing.Size(75, 23);
+            this.KPButton.Size = new System.Drawing.Size(56, 19);
             this.KPButton.TabIndex = 7;
             this.KPButton.Text = "Set KP";
             this.KPButton.UseVisualStyleBackColor = true;
@@ -211,9 +224,10 @@
             // 
             // KDButton
             // 
-            this.KDButton.Location = new System.Drawing.Point(685, 121);
+            this.KDButton.Location = new System.Drawing.Point(526, 93);
+            this.KDButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.KDButton.Name = "KDButton";
-            this.KDButton.Size = new System.Drawing.Size(75, 23);
+            this.KDButton.Size = new System.Drawing.Size(56, 19);
             this.KDButton.TabIndex = 8;
             this.KDButton.Text = "Set KD";
             this.KDButton.UseVisualStyleBackColor = true;
@@ -221,9 +235,10 @@
             // 
             // KIButton
             // 
-            this.KIButton.Location = new System.Drawing.Point(685, 151);
+            this.KIButton.Location = new System.Drawing.Point(526, 118);
+            this.KIButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.KIButton.Name = "KIButton";
-            this.KIButton.Size = new System.Drawing.Size(75, 23);
+            this.KIButton.Size = new System.Drawing.Size(56, 19);
             this.KIButton.TabIndex = 9;
             this.KIButton.Text = "Set KI";
             this.KIButton.UseVisualStyleBackColor = true;
@@ -232,36 +247,39 @@
             // KPBox
             // 
             this.KPBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.KPBox.Location = new System.Drawing.Point(589, 91);
+            this.KPBox.Location = new System.Drawing.Point(454, 69);
+            this.KPBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.KPBox.Name = "KPBox";
-            this.KPBox.Size = new System.Drawing.Size(90, 24);
+            this.KPBox.Size = new System.Drawing.Size(68, 20);
             this.KPBox.TabIndex = 10;
             this.KPBox.Text = "";
             // 
             // KDBox
             // 
             this.KDBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.KDBox.Location = new System.Drawing.Point(589, 121);
+            this.KDBox.Location = new System.Drawing.Point(454, 93);
+            this.KDBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.KDBox.Name = "KDBox";
-            this.KDBox.Size = new System.Drawing.Size(90, 24);
+            this.KDBox.Size = new System.Drawing.Size(68, 20);
             this.KDBox.TabIndex = 10;
             this.KDBox.Text = "";
             // 
             // KIBox
             // 
             this.KIBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.KIBox.Location = new System.Drawing.Point(589, 151);
+            this.KIBox.Location = new System.Drawing.Point(454, 118);
+            this.KIBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.KIBox.Name = "KIBox";
-            this.KIBox.Size = new System.Drawing.Size(90, 24);
+            this.KIBox.Size = new System.Drawing.Size(68, 20);
             this.KIBox.TabIndex = 10;
             this.KIBox.Text = "";
             // 
             // Quadrocopter
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(879, 752);
+            this.ClientSize = new System.Drawing.Size(659, 611);
             this.Controls.Add(this.KIBox);
             this.Controls.Add(this.KDBox);
             this.Controls.Add(this.KPBox);
@@ -282,6 +300,7 @@
             this.Controls.Add(this.StatusLabel);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
             this.Name = "Quadrocopter";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
